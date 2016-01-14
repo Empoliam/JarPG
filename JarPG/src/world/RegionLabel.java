768147fa0037d321e10a40f08286b4e3a07ef5ab
Empoliam <liamjwright@btinternet.com>
2016-01-14 21:40:10 +0000

    Started CCL algorithm
    -Got bored and time ran out, so nothing much

---
+++ JarPG/src/world/RegionLabel.java
@@ -0,0 +1,19 @@
+package world;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegionLabel 
+{
+
+	int[][] label;
+	List<Integer> labellist = new ArrayList<Integer>();
+	
+	public RegionLabel(int[][] datain, int size)
+	{
+		
+		label = new int[size][size];
+		
+	}
+	
+}
